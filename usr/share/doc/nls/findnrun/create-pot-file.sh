ea5b8c5aef74ba35e2fb5470622f34f8eaff9c39
step- <step-@users.noreply.github.com>
2016-03-11 18:24:47 +0100

    i18n .pot template build tools.

---
+++ usr/share/doc/nls/findnrun/create-pot-file.sh
@@ -0,0 +1,172 @@
+#!/bin/sh
+
+# =============================================================================
+# Create findnrun GNU GetText template file
+  Version=1.0.0
+# author: Copyright (C)2016 step
+# license: GNU GPL applies
+# depends: GNU gettext package, mdview
+# source: https://github.com/step-/find-n-run
+# forum: http://www.murga-linux.com/puppy/viewtopic.php?t=102811
+# =============================================================================
+
+# Stubs for configuration file.
+is_to_be_translated() { :; }
+list_md_files() { :; }
+FPOT= XSRC= XOPT=
+
+CONFIG=$1
+[ -z "$CONFIG" ] && CONFIG="${0%.sh}-config.sh"
+if ! [ -r "$CONFIG" ]; then
+  echo >&2 "${0##*/}: error: can't read configuration file '$CONFIG'.
+usage: ${0##*/} [config-file (default ${0%.sh}-config.sh)]"
+  exit 1
+fi
+. "$CONFIG"
+if ! [ -e "$XSRC" -a -n "$FPOT" ]; then
+  echo >&2 "${0##*/}: error: at least one of XSRC, FPOT is wrong."
+  exit 1
+fi
+
+mdview=${MDVIEW:-mdview}
+mdview=$(command -v "$mdview")
+if [ -z "$mdview" ]; then
+  echo >&2 "${0##*/}: error: can't find command 'mdview'. Found MDVIEW='$MDVIEW'."
+  exit 1
+fi
+
+create_pot_file() # $1-pot-file $2...-xgettext-options {{{1
+{
+  local f x fpot xopt
+  fpot=$1; shift
+  rm -f "$fpot.tmp"
+
+  # Create pot file header
+  init_po_file "$fpot.tmp" || {
+    ERRORS="${ERRORS}
+    init_po_file '$fpot' $*"; return 1; }
+  # Append all notes
+  insert_notes >> "$fpot.tmp"
+  # Append main source file scan
+  scan_source_file "$XSRC" "$@" --omit-header >> "$fpot.tmp" || {
+    ERRORS="${ERRORS}
+    scan_source_file '$XSRC'"; return 1; }
+  # Append notes again
+  type __notes_on_pot_file >/dev/null 2>&1 &&
+    __notes_on_pot_file >> "$fpot.tmp"
+  # Append .md file scans
+  while read -r f; do
+    is_to_be_translated "$f" || continue
+    scan_md_file "$f" "$fpot.2.tmp"
+    # Cumulate unique messages, all comments and all file positions.
+    msgcat --no-wrap -o "$fpot.tmp" "$fpot.tmp" "$fpot.2.tmp" || ERRORS="${ERRORS}
+    msgcat '$f'"
+  done << EOF
+  $(list_md_files)
+EOF
+  # Delete annoyances
+  sed '
+/#-#-#-#-#/d
+s~^#: ../../../../~#: /usr/~
+s~^#:.*'"$PACKAGE_NAME"'~#: ~
+' "$fpot.tmp" || ERRORS="${ERRORS}
+  sed"
+
+  rm  -f "$fpot."*tmp
+  return ${ERRORS:+1}
+}
+
+scan_source_file() # $1-filepath $2...-xgettext-options {{{1
+{
+  local f
+  f=$1; shift &&
+  env TZ="$PACKAGE_POT_CREATION_TZ" \
+    xgettext "$@" --no-wrap -o -  \
+      --package-name="$PACKAGE_NAME" \
+      --package-version="$PACKAGE_VERSION" \
+      --msgid-bugs-address="$PACKAGE_POT_BUGS_ADDRESS" \
+    "$f"
+}
+
+init_po_file() # $1-po(t)-OUTPUT-file $2...-xgettext-options {{{1
+{
+  local f
+  f=$1; shift
+  env TZ="$PACKAGE_POT_CREATION_TZ" \
+    xgettext "$@" --force-po -C -o "$f" \
+      --package-name="$PACKAGE_NAME" \
+      --package-version="$PACKAGE_VERSION" \
+      --msgid-bugs-address="$PACKAGE_POT_BUGS_ADDRESS" \
+    /dev/null &&
+  sed -i '
+1,20{s~SOME DESCRIPTIVE TITLE~'"$PACKAGE_TITLE"'~}
+1,20{s~YEAR THE PACKAGE.*$~'"$PACKAGE_COPYRIGHT"'~}
+1,20{s~FIRST AUTHOR.*$~'"$PACKAGE_FIRST_POT_AUTHOR"'~}
+1,20{s~Language: ~&'"$PACKAGE_POT_LANGUAGE"'~}
+1,20{s~=CHARSET~='"$PACKAGE_CHARSET"'~}
+$ a \
+"Plural-Forms: nplurals=INTEGER; plural=EXPRESSION;\\n"\
+
+' "$f"
+}
+
+insert_notes() # {{{1
+{
+  local f
+  # Opening notes on pot file
+  type __notes_on_pot_file >/dev/null 2>&1 &&
+    __notes_on_pot_file
+  # Call all function names that start with '__notes_on_file' in
+  # config file except those that are marked 'excluded'.
+  for f in $(awk -F '[ \t()]' \
+    '/^__notes_on_file/ && ! /excluded/ {print $1}' "$CONFIG"); do
+    eval $f
+  done
+}
+
+insert_notes_on_file() # $1-filepath {{{1
+{
+  local f x
+  f=$1
+  echo "
+#.
+#. ---------------------------------------------------------
+#. i18n $f
+#. ---------------------------------------------------------
+#.
+"
+  x=__notes_on_file_${f##*/}; x=${x%.*}
+  type "$x" >/dev/null 2>&1 && eval "$x"
+}
+
+scan_md_file() # $1-in-filepath $2-out-filepath {{{1
+{
+  local in out
+  in=$1 out=$2
+  echo >&2 "$in"
+
+  init_po_file "$out" || {
+    ERRORS="${ERRORS}
+    init_po_file '$in'"; return 1; }
+
+  insert_notes_on_file "$in" >> "$out"
+
+  $mdview --po "$in" >> "$out" # || {
+    #ERRORS="${ERRORS}
+    #$mdview '$in'"; return 1; }
+  # FIXME[mdview] mdview's stdout_output() doesn't exit(code) at all
+
+  # Always run msguniq on output by mdview --po
+  msguniq --no-wrap -i "$out" -o "$out" || {
+    ERRORS="${ERRORS}
+   msguniq '$in'"; return 1; }
+}
+
+# {{{1}}}
+ERRORS=
+create_pot_file "$FPOT" $XOPT > "$FPOT"
+if [ -n "${ERRORS}" ]; then
+  echo >&2 "${0##*/}: ERRORS:${ERRORS}"
+  exit 1
+fi
+
---
+++ usr/share/doc/nls/findnrun/findnrun-pot-config.sh
@@ -0,0 +1,107 @@
+# This file is sourced not run
+
+# Optional settings {{{1
+MDVIEW=./mdview
+
+# Required script settings {{{1
+# Output file name
+FPOT=findnrun.new.pot
+# $XSRC source file is scanned for msgids with xgettext $XOPT
+XSRC=../../../../bin/findnrun
+XOPT="-ci18n -L Shell"
+# Multiple files in list_md_files() are scanned for msgids with mdview --po {{{2
+list_md_files()
+{
+  local d=../../findnrun
+  set +f
+  printf "%s\n" "$d/"*.md "$D/../examples/"*/*.md
+}
+
+# A file failing is_to_be_translated() isn't even scanned {{{2
+is_to_be_translated() # $1-filepath
+{
+  case $f in
+    */TRANSLATING.md)
+      : $f targets translators - they need English doc only
+      ;;
+    */DEBIAN.md)
+      : $f Debian package is not maintained since version 2.0.0
+      ;;
+    */plugin-debugging.md|*/plugin-dev.md|*/plugin-examples.md)
+      : $f targets developers - they prefer English doc anyway
+      # but plugin.md and plugin-list.md are to be translated
+      ;;
+    */no-help.md)
+      : $f is obsolete
+      ;;
+    */examples/*)
+      : $f not now - maybe to be translated in a future release
+      ;;
+    */LICENSE.md)
+      : we keep $f in English as GNU GPL does not endorse translations
+      ;;
+    *) # translate all other files
+      : $f target end users - translate it
+      return 0
+      ;;
+  esac
+  return 1
+}
+
+# Required project header settings {{{1
+PACKAGE_VERSION="2.1.0"
+PACKAGE_NAME="find-n-run"
+PACKAGE_TITLE="Findnrun $PACKAGE_VERSION"
+PACKAGE_COPYRIGHT="2015-2016 step; 2015 FSH, L18L, step"
+PACKAGE_FIRST_POT_AUTHOR="step, 2015 https://github.com/step-/"
+PACKAGE_POT_CREATION_TZ="UTC"
+PACKAGE_POT_LANGUAGE="en"
+PACKAGE_CHARSET="UTF-8"
+PACKAGE_POT_BUGS_ADDRESS="https://github.com/step-/find-n-run/issues/"
+
+__notes_on_pot_file() # {{{1
+{
+  echo '
+#.
+#. =======================================================================
+#. i18n User documentation in markdown format from .md files follows.
+#. i18n IMPORTANT! Reproduce indentation and special punctuation exactly.
+#. =======================================================================
+#.
+'
+}
+
+
+__notes_on_file_LICENSE() #{{{1 # is excluded from the pot file
+{
+  echo "
+#. i18n File LICENSE.md is a special case. Do not to translate it. Instead
+#. i18n find a translation from http://www.gnu.org/licenses/translations.html
+#. i18n and use that translated text here.
+#.
+"
+}
+
+__notes_on_file_preference() # {{{1
+{
+  echo "
+#. i18n File 'preference.md' includes markdown CODE BLOCKS. Generally, you
+#. i18n don't not need to translate code blocks but for file preference.md
+#. i18n you do because they are shell comments that tell the user what
+#. i18n function each setting performs. So, INSIDE code blocks you will
+#. i18n translate the COMMENT lines before each variable assignment.
+#. i18n Make sure to keep the comment '#' hash character where it is.
+#. i18n The source text can include MULTIPLE-LINE shell comments. In such
+#. i18n case each line is presented for translation as a separate msgid,
+#. i18n which can be confusing. Look at the source file to understand over
+#. i18n which set of msgids the shell comment spreads. Your translation
+#. i18n needs to spread over the same set exactly. You may choose to format
+#. i18n your comment differently from the source, for example to even out
+#. i18n line lengths. Make sure to keep the same set of msgids and the '#'
+#. i18n hash character at the beginning of each translated msgstr.
+#. i18n Do NOT translate VARIABLE names or variable assignments.
+#. i18n And, as usual, translate all text outside the code blocks.
+#.
+"
+}
+
